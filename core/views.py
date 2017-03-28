31718110bfda5c651893ff86c92238786853de05
Harold Wanyama <wanyaland@gmail.com>
2017-03-28 15:07:45 +0300

    removed customer form in sign_up

--- core/views.py
+++ core/views.py
@@ -280,10 +280,9 @@ def sign_up_moderator(request):
 def sign_up(request):
     if request.method=='POST':
         form = RegistrationForm(request.POST)
-        customer_form = CustomerForm(request.POST,request.FILES)
-        if  form.is_valid() and customer_form.is_valid() :
+        if form.is_valid() :
             user = form.save()
-            customer = customer_form.save(commit=False)
+            customer = Customer.objects.get(user=user)
             customer.user = user
             customer.user_type = Customer.CUSTOMER
             user.save()
@@ -291,9 +290,8 @@ def sign_up(request):
             return redirect('core:home')
     else:
         form = RegistrationForm()
-        customer_form = CustomerForm()
     return render(request,'core/auth-user/sign_up.html',{
-        'form':form,'customer_form':customer_form
+        'form':form
     })
 
 
@@ -829,4 +827,6 @@ def combineEventDateAndTime(form):
         form.instance.end_date = None
 
     return form
+
+
 
