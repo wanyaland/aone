0675e79aa91fc9f81b9f0dfa49e59f60210b62a7
Deepak Verma <Deepak.Verma@mediaagility.com>
2017-09-29 23:12:34 +0530

    raising not implemented error

--- core/middleware.py
+++ core/middleware.py
@@ -4,6 +4,7 @@ from analytics import AnalyticsWrapper
 class MasterMiddleware(object):
     """
     Instead of having multiple middleware, create a middleware with different utility methods
+    TODO: this is not completed yet
     """
     def __init__(self, get_response):
         self.get_response = get_response
@@ -18,6 +19,4 @@ class MasterMiddleware(object):
 
         :return:
         """
-        pass
-
-    def
+        raise NotImplementedError()
