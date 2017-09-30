74d24c6b2eab9789841a19b868de105fd3c53dc8
Deepak Verma <Deepak.Verma@mediaagility.com>
2017-09-30 21:32:17 +0530

    fix for app label

--- africaone/settings.py
+++ africaone/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.home',
-    'apps.feed',
+    'app.home',
+    'app.feed',
 ]
 
 MIDDLEWARE = [
