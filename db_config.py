6bed929e3fcd1aae10d3ada9917886b2418c89b1
Harold <wanyaland@gmail.com>
2017-10-12 23:25:31 +0200

    config

--- db_config.py
+++ db_config.py
@@ -3,6 +3,6 @@ import os
 if os.environ.get('ENVIRONMENT', 'DEV') == 'DEV':
     os.environ['DB_NAME'] = 'africaone'
     os.environ['DB_USER'] = 'root'
-    os.environ['DB_PASSWORD'] = 'dverma'
+    os.environ['DB_PASSWORD'] = 'root'
     os.environ['DB_HOST'] = '127.0.0.1'
     os.environ['DB_PORT'] = '3306'
